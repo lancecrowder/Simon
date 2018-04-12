867c7c56b7dacf2d9125f32fc5b71b1828ab528f
lancecrowder <thelancecrowder@gmail.com>
2018-04-12 15:11:36 -0500

    Simon animation created

---
+++ simonJS.js
@@ -0,0 +1,10 @@
+// $('.box').click(function () {
+//     console.log('click');
+//     $(this).animate({
+//         opacity: 0.1
+//     }, 500, function () {
+//         $(this).animate ({
+//             opacity: 1
+//         });
+//     });
+// });
